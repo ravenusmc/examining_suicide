d7b472e2e48f52cc8c3dde08b8c5ae6e23516c28
ravenusmc <mcuddy77@gmail.com>
2019-01-03 19:53:29 -0500

    added start code for world map

--- client/src/components/graphs/Graph.vue
+++ client/src/components/graphs/Graph.vue
@@ -1,50 +1,85 @@
 <template>
   <div>
     <h1>Graph Page</h1>
-    <vue-chart
+    <!-- <vue-chart
     chart-type="BarChart"
+    :columns="columns"
+    :rows="rows"
+    :options="options"
+></vue-chart> -->
+<vue-chart
+    chart-type="GeoChart"
     :columns="columns"
     :rows="rows"
     :options="options"
 ></vue-chart>
   </div>
+
 </template>
 <script>
 export default {
-  name: 'Graph',
-  data: function () {
-           return {
-               columns: [{
-                   'type': 'string',
-                   'label': 'Year'
-               }, {
-                   'type': 'number',
-                   'label': 'Suicide'
-               }],
-               rows: [
-                   ['2004', 1000],
-                   ['2005', 1170],
-                   ['2006', 660],
-                   ['2007', 1030]
-               ],
-               options: {
-                   title: 'Suicides by Year',
-                   hAxis: {
-                       title: 'Money',
-                       minValue: '2004',
-                       maxValue: '2007'
-                   },
-                   vAxis: {
-                       title: 'Year',
-                       minValue: 500,
-                       maxValue: 1200
-                   },
-                   width: 900,
-                   height: 500,
-               }
-           }
-       },
-};
+    data: function () {
+        return {
+            columns: [{
+                'type': 'string',
+                'label': 'Country'
+            }, {
+                'type': 'number',
+                'label': 'Suicides'
+            }],
+            rows: [
+                ['Albania', 200],
+                ['United States', 300],
+                ['Brazil', 400],
+                ['Canada', 700],
+                ['France', 201]
+            ],
+            options: {
+                title: 'Suicide by Countries',
+                width: 900,
+                height: 500,
+            }
+        }
+    },
+}
+
+
+
+// export default {
+//   name: 'Graph',
+//   data: function () {
+//            return {
+//                columns: [{
+//                    'type': 'string',
+//                    'label': 'Year'
+//                }, {
+//                    'type': 'number',
+//                    'label': 'Suicide'
+//                }],
+//                rows: [
+//                    ['2004', 1000],
+//                    ['2005', 1170],
+//                    ['2006', 660],
+//                    ['2007', 1030]
+//                ],
+//                options: {
+//                    title: 'Suicides by Year',
+//                    hAxis: {
+//                        title: 'Money',
+//                        minValue: '2004',
+//                        maxValue: '2007'
+//                    },
+//                    vAxis: {
+//                        title: 'Year',
+//                        minValue: 500,
+//                        maxValue: 1200
+//                    },
+//                    width: 900,
+//                    height: 500,
+//                }
+//            }
+//        },
+// };
 </script>
 <style>
 </style>
